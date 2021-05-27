5b17127157c6236f8d11bb94183eca3887741e28
SeldereyTM <55454980+SeldereyTM@users.noreply.github.com>
2021-05-27 17:19:03 +0300

    Add files via upload

---
+++ Laba1.java
@@ -0,0 +1,31 @@
+
+public class Laba1 {
+
+    static class Mouse {
+        public Brand brand;
+        String id;
+        Specifications specifications;
+    }
+
+    static class Specifications{
+        double weight;
+        boolean processor, silentButtons;
+        ConnectionType connectionType;
+        Demensions demensions;
+        int numberOfButtons;
+    }
+
+    static class Demensions{
+        double height, width;
+    }
+
+    enum Brand{
+        A4Tech, DEXP, Defender, Oklick, Smartbuy, Logitech
+    }
+    enum ConnectionType{
+        Wireless, Wired, Combined
+    }
+
+
+}
+
